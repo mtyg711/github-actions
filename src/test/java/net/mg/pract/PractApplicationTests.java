8b17730b589cbe3b0b44420685f89188e415e0ff
Murthy <mgarimella@learnbeyond.com>
2025-01-11 16:14:02 -0500

    intial commit

---
+++ src/test/java/net/mg/pract/PractApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mg.pract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
