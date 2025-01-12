b66abd6107c578cc25bbaa7d9111394816abbedf
Murthy <mgarimella@learnbeyond.com>
2025-01-11 23:08:11 -0500

    Updated workflow

--- src/main/java/net/mg/pract/controller/UserController.java
+++ src/main/java/net/mg/pract/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String name(){
-        return "Hello!";
+        return "Hello!!";
     }
 
 }
